4f6b3fdfd377945cbae147c24bb0d723783fc078
Arseniy <plakhotniuk.av@phystech.edu>
2023-12-19 16:54:40 +0300

    init

--- tests/cmake-build-debug/googletest-master/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/arseniy/study/CompMech/ballistics-plakhotniuk-arseniy/tests/googletest-master/googletest
-# Build directory: /Users/arseniy/study/CompMech/ballistics-plakhotniuk-arseniy/tests/cmake-build-debug/googletest-master/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
